cc8e146cdd8b153606d7452cd8bdaa24d3380cac
arashk <yadegar.arashk@gmail.com>
2025-02-19 10:05:22 -0800

    deleted dist folder

--- websocket-server/dist/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Messages = void 0;
-var Messages;
-(function (Messages) {
-    Messages["RECIEVED_JSON"] = "Received JSON";
-    Messages["RECIEVED_FILE"] = "Received File";
-    Messages["RECIEVED_ERROR"] = "Received Error";
-})(Messages || (exports.Messages = Messages = {}));
